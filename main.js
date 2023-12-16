d6672ffb0a4defe7c9773d554d89303188330e9e
Tyler Carnemolla <tymolla53@gmail.com>
2023-12-16 10:42:50 -0800

    second commit

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-    // Your Coinlayer API credentials
+ 
     let endpoint = 'live';
     let access_key = '46ac6145823187ccb701cb1763e43d08';
 
-    // Function to make the API request and update the HTML
+    
     function get_live(figureId) {
-        // Check if data is already in localStorage
         let storedData = localStorage.getItem('cryptoData');
 
         if (storedData) {
-            // If data is present, use it directly
             updateHTML(figureId, JSON.parse(storedData));
         } else {
-            // If data is not present, make the API request
             let apiUrl = `https://api.coinlayer.com/api/${endpoint}?access_key=${access_key}`;
 
             fetch(apiUrl)
                 .then(response => response.json())
                 .then(data => {
-                    // Update the HTML with the received data
                     updateHTML(figureId, data);
-
-                    // Store the data in localStorage for future use
                     localStorage.setItem('cryptoData', JSON.stringify(data));
                 })
                 .catch(error => {
@@ -33,19 +27,14 @@
     function updateHTML(figureId, data) {
         let figure = document.getElementById(figureId);
 
-        // Assuming you have a specific structure for your live data
-        // Modify this part according to your data structure
         let htmlContent = `<table>
                               <thead>
                                 <tr>
                                   <th>Crypto</th>
                                   <th>Price</th>
-                                  <!-- Add more columns as needed -->
                                 </tr>
                               </thead>
                               <tbody>`;
-
-        // Loop through the data and create rows
         for (let crypto in data.rates) {
             htmlContent += `<tr>
                               <td>${crypto}</td>
@@ -56,25 +45,19 @@
 
         htmlContent += `</tbody></table>`;
 
-        // Set the HTML content of the figure element
         figure.innerHTML = htmlContent;
     }
 
-    // Function to stop the live stream
-    function stopfeed() {
-        // Implement the logic to stop the live stream if needed
-        console.log('Stopping Crypto Live Stream...');
-    }
 
     // Call get_live when the page loads
     window.onload = function () {
         get_live('fig1');
     };
 
-    // now for the past data query
-
    
 
+   
+    // now for the past data query
     // Function to make the API request for past data on a specified date
     function past_value(figureId, event) {
         event.preventDefault();
@@ -98,11 +81,9 @@
             fetch(apiUrl)
                 .then(response => response.json())
                 .then(data => {
-                    // Check if the cryptocurrency exists in the response
                     if (data.rates && data.rates.hasOwnProperty(cryptoName)) {
                         let pastValue = data.rates[cryptoName];
     
-                        // Display the past value in the past value table
                         pastValueTable.innerHTML = `<thead>
                                                        <tr>
                                                          <th>Crypto</th>
@@ -140,10 +121,8 @@
         let amount = parseFloat(amountInput.value.trim());
 
         if (fromCurrency && toCurrency && !isNaN(amount) && amount > 0) {
-            // Your Coinlayer API access key
             let accessKey = '46ac6145823187ccb701cb1763e43d08';
 
-            // Make the API request for currency conversion
             let apiUrl = `https://api.coinlayer.com/convert?access_key=${accessKey}&from=${fromCurrency}&to=${toCurrency}&amount=${amount}`;
             let ConversionFigure = document.querySelector('.conversion_table');
             console.log('ConversionFigure:', ConversionFigure);
